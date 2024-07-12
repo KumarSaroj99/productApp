2647281cb07bb93887e648a485d54ee806b2a6e3
KumarSaroj99 <sarojkumar.panda@aurionpro.com>
2024-07-12 21:56:53 +0530

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApp.Modules
+{
+    internal class Product
+    {
+        private int _id;
+        private string _name;
+        private int _price;
+        private int _discountPercentage;
+
+        //Seters
+        public void SetId(int id)
+        {
+            _id = id;
+        }
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+        public void SetPrice(int price)
+        {
+            _price = price;
+        }
+        public void SetDiscount(int discountPercentage)
+        {
+            _discountPercentage = discountPercentage;
+        }
+
+        //Geters
+        public int GetId()
+        {
+            return _id;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+        public int GetPrice()
+        {
+            return _price;
+        }
+        public int GetDiscout()
+        {
+            return _discountPercentage;
+        }
+        //To calculate Final price after discount
+        public float FinalPriceAfterDiscount()
+        {
+            return _price - _discountPercentage / 100F * _price;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductApp.Modules;
+
+namespace ProductApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //First Product
+            Product product1 = new Product();
+            SetDetails(product1,1,"Book",50,10 );
+
+            //Second Product
+            Product product2 = new Product();
+            SetDetails(product2, 2, "School bag", 300, 15);
+
+            //To display First Product Detail
+            ShowProductDetails(product1);
+
+            //To Separate the products
+            Console.WriteLine("=================================");
+
+            //To display Second Product Detail
+            ShowProductDetails(product2);
+
+
+        }
+        //Method to display Details of product
+        static void ShowProductDetails(Product product)
+        {
+            Console.WriteLine("Product Id : " + product.GetId());
+            Console.WriteLine("Product Name : "+product.GetName());
+            Console.WriteLine("Product Actual Price : "+product.GetPrice());
+            Console.WriteLine("Discount Percentage on product : "+product.GetDiscout()+"%");
+            Console.WriteLine("Product final price after discount : "+product.FinalPriceAfterDiscount());
+
+        }
+        static void SetDetails(Product product, int id, string name, int price, int discount)
+        {
+            product.SetId(id);
+            product.SetName(name);
+            product.SetPrice(price);
+            product.SetDiscount(discount);
+        }
+
+    }
+}
